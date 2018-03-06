eb84ab7fa741c2c63ce84e97942b7c07a73017b5
splashofcrimson <splashofcrimson@gmail.com>
2018-03-06 12:43:16 -0800

    quick fix

---
+++ src/snow.py
@@ -0,0 +1,9 @@
+# Snow falls unevenly on Cornell's campus.
+# Your objective is, given a dictionary of Cornell
+# buildings and their respective snowfall in inches,
+# return the building that experienced the most snowfall
+# Example input: {Uris: 5, Gates: 3.4, Philips: 7.9}
+# Example output: Philips
+
+def snow(buildings):
+    pass
