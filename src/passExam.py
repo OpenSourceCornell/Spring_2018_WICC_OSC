a5e462d99b99d895f6718d8a1c9e5f41b1634130
ujnomw <m.golovatskih@gmail.com>
2018-03-10 15:49:25 +0300

    PassExam implementation

--- src/passExam.py
+++ src/passExam.py
@@ -2,4 +2,6 @@
 #a student passes their exam if they study for more than 5 hours but less
 #than 40 (at 40, their brain turns to mush)
 def pass_exam(hours_studied):
-	pass
+	if hours_studied < 40 and 5 < hours_studied:
+		return True
+	return False	
