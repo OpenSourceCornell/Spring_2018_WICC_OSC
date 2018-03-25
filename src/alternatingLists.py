cc6a75ebbc53d1a94f797b5355e3a4548591b13e
ammar0211 <32436267+ammar0211@users.noreply.github.com>
2018-03-25 19:23:44 +0530

    Update alternatingLists.py

--- src/alternatingLists.py
+++ src/alternatingLists.py
@@ -3,4 +3,14 @@
 #ex: ['a','b','c','d'], ['1','2']
 #return: ['a','1','b','2','c','d']
 def alternating_lists(list1, list2):
-	pass
+	Alt_list=[]
+	if len(list1)>len(list2):
+		for i in range(len(list1)):
+			if i<len(list2): Alt_list.extend([list1[i],list2[i]])
+			else: Alt_list.append(list1[i])
+	else:
+		for i in range(len(list2)):
+			if i<len(list1): Alt_list.extend([list1[i],list2[i]])
+			else: Alt_list.append(list2[i])
+	return Alt_list
+    
